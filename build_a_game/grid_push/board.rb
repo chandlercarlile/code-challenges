a3060a2decc778f36dcbe80a136dd47fdd082ebb
chandlercarlile <chandler.carlile@gmail.com>
2023-10-24 20:10:31 -0700

    box - only one box - can be moved - can "run over" coins

--- build_a_game/grid_push/board.rb
+++ build_a_game/grid_push/board.rb
@@ -1,5 +1,6 @@
 require "./player_character"
 require "./coin"
+require "./box"
 
 class Board
   CELL = "."
@@ -18,6 +19,7 @@ class Board
     # TODO:
     # - more than one coin can spawn?
     spawn(type: "Coin")
+    spawn(type: "Box")
   end
 
   def print_board
@@ -55,10 +57,26 @@ class Board
       else
         spawn(type: "Coin")
       end
+    when "Box"
+      if @grid[location_y.to_i][location_x.to_i].empty?
+        @box = ::Box.new(location_x: location_x, location_y: location_y)
+        @grid[location_y.to_i][location_x.to_i] << @box
+      else
+        spawn(type: "Box")
+      end
     end
   end
 
   def move_player_character(distance_x:, distance_y:)
+    # assume that the player can only move one space at a time
+    # this is a helper to get direction as human readable
+    direction =
+      distance_x == 1 ? "e" :
+      distance_x == -1 ? "w" :
+      distance_y == 1 ? "n" :
+      distance_y == -1 ? "s" :
+      "?"
+
     # TODO:
     # - make a move command, which takes the object that you want to move
     # - this command just calls move with the PC
@@ -70,6 +88,7 @@ class Board
     # check if the player can perform that move on the board
     # - nothing to do here yet, player can move unrestricted
     # - easy way to do this, check everything else on that tile for "movable" or "collectible"
+    # - for boxes/things that can be pushed in a chain, check each thing for "movable", else revert
 
     # if yes, move the character
     @grid[@pc.location_y][@pc.location_x] -= [@pc]
@@ -79,8 +98,11 @@ class Board
 
     @grid[new_location_y][new_location_x] << @pc
 
-    # collect anything that is "collectible"
     items = @grid[new_location_y][new_location_x]
+
+    # collect anything that is "collectible" & move anything that is "movable"
+    # - this may be easier if we have the direction the player was moving (as an argument)
+    # - otherwise we will need to calculate it
     items.each do |item|
       next if item == @pc
 
@@ -89,10 +111,31 @@ class Board
         @grid[new_location_y][new_location_x] -= [@coin]
         spawn(type: "Coin")
       end
-    end
 
-    # move anything that is "movable"
-    # - this may be easier if we have the direction the player was moving (as an argument)
-    # - otherwise we will need to calculate it
+      if item == @box
+        # calculate new box location
+        new_box_location_x = (new_location_x + distance_x) % width
+        new_box_location_y = (new_location_y + distance_y) % height
+
+        # check if box can be moved there
+        # - nothing to do here, box can move unrestricted
+        # - eventually this can be a more "global" method, where you pass the item to move
+        # - and the board tells you if it can move there or not
+
+        # check if box ran over the coin
+        # - if so, subtract score and spawn new coin
+        if @grid[new_box_location_y][new_box_location_x] == [@coin]
+          @score -= 1
+          @grid[new_box_location_y][new_box_location_x] -= [@coin]
+          spawn(type: "Coin")
+        end
+
+        # now that the space is for sure empty, we move the box
+        @grid[new_location_y][new_location_x] -= [@box]
+        @box.location_x = new_box_location_x
+        @box.location_y = new_box_location_y
+        @grid[new_box_location_y][new_box_location_x] << @box
+      end
+    end
   end
 end

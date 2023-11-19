b797df620dbbd750d22b41add5cee3745e50e887
chandlercarlile <chandler.carlile@gmail.com>
2023-11-19 14:24:10 -0800

    add collected to coins

--- build_a_game/part-5-grid_stop/coin.rb
+++ build_a_game/part-5-grid_stop/coin.rb
@@ -8,12 +8,13 @@ class Coin < Thing
   COLOR = "93m"
   ICON = " " + ESCAPE + COLOR + "\u25CB" + CLEAR_CODE + " "
 
-  attr_accessor :location_x, :location_y, :icon
+  attr_accessor :location_x, :location_y, :icon, :collected
 
   def initialize(location_x:, location_y:)
     @location_x = location_x.to_i
     @location_y = location_y.to_i
 
     @icon = ICON
+    @collected = false
   end
 end
